a4dec8502bb3582c406542662c7fbdb3a3044623
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2023-12-13 01:35:17 +0530

    Update initialization.py

--- includes/bin/initialization.py
+++ includes/bin/initialization.py
@@ -5,9 +5,8 @@ target_name = sys.argv[1]
 savepath = sys.argv[2]
 
 # Creating folder for data
-data_folder = os.path.join(savepath, 'data')
-os.makedirs(data_folder, exist_ok=True)
+os.makedirs(savepath, exist_ok=True)
 
 # Creating folder with company name
-company_folder = os.path.join(data_folder, target_name)
+company_folder = os.path.join(savepath, target_name)
 os.makedirs(company_folder, exist_ok=True)
